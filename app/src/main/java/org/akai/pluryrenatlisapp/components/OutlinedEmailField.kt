8ee2589e2bb90466336377b2d342a90a1ff936d8
Maksymilian Ryder <77546701+McSymilian@users.noreply.github.com>
2024-08-15 21:36:06 +0200

    unused imports removed

--- app/src/main/java/org/akai/pluryrenatlisapp/components/OutlinedEmailField.kt
+++ app/src/main/java/org/akai/pluryrenatlisapp/components/OutlinedEmailField.kt
@@ -1,21 +1,16 @@
 package org.akai.pluryrenatlisapp.components
 
 import android.util.Patterns
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material.icons.rounded.Info
-import androidx.compose.material.icons.rounded.Warning
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,7 +21,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.akai.pluryrenatlisapp.ui.theme.getDefaultOutlinedTextFieldColors
