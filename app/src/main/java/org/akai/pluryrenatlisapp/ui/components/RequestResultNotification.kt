6d681ed8e6b87df74535eada0d96f7a7e7f7c1a9
Maksymilian Ryder <77546701+McSymilian@users.noreply.github.com>
2024-08-19 23:53:56 +0200

    Request Result Notification basic parametrization

--- app/src/main/java/org/akai/pluryrenatlisapp/ui/components/RequestResultNotification.kt
+++ app/src/main/java/org/akai/pluryrenatlisapp/ui/components/RequestResultNotification.kt
@@ -1,23 +1,33 @@
 package org.akai.pluryrenatlisapp.ui.components
 
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.akai.pluryrenatlisapp.ui.theme.PluryRenatlisAppTheme
 
 @Composable
-fun RequestResultNotification() {
-    Card {
+fun RequestResultNotification(
+    modifier: Modifier = Modifier,
+    text: String = "Wybrane rzeczy zostały pomyślnie wypożyczone",
+    backgroundColor: Color = MaterialTheme.colorScheme.tertiaryContainer
+) {
+    Card (
+        modifier = modifier,
+        colors = CardDefaults.cardColors(
+            containerColor = backgroundColor
+        ),
+        shape = MaterialTheme.shapes.extraLarge
+    ) {
         Text(
-            text = "Wybrane rzeczy zostały pomyślnie wyporzyczone",
-            minLines = 2,
+            text = text,
             fontSize = MaterialTheme.typography.titleLarge.fontSize,
             modifier = Modifier.padding(32.dp),
             textAlign = TextAlign.Center
