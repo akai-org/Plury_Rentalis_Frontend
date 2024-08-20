26d288e2804612be39625731e2bc1686ac8dd4f6
Maksymilian Ryder <77546701+McSymilian@users.noreply.github.com>
2024-08-20 14:54:04 +0200

    main nav host implementation

--- app/src/main/java/org/akai/pluryrenatlisapp/MainActivity.kt
+++ app/src/main/java/org/akai/pluryrenatlisapp/MainActivity.kt
@@ -7,10 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import org.akai.pluryrenatlisapp.navhost.MainNavHost
 import org.akai.pluryrenatlisapp.ui.theme.PluryRenatlisAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PluryRenatlisAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MainNavHost(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +27,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PluryRenatlisAppTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/org/akai/pluryrenatlisapp/navhost/MainNavHost.kt
+++ app/src/main/java/org/akai/pluryrenatlisapp/navhost/MainNavHost.kt
@@ -1,24 +1,34 @@
 package org.akai.pluryrenatlisapp.navhost
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import kotlinx.coroutines.launch
 import org.akai.pluryrenatlisapp.apiclient.Authorization
+import org.akai.pluryrenatlisapp.apiclient.AuthorizationViewModel
 import org.akai.pluryrenatlisapp.data.User
 import org.akai.pluryrenatlisapp.ui.compositions.HomeComposition
 import org.akai.pluryrenatlisapp.ui.compositions.LoginComposition
 import org.akai.pluryrenatlisapp.ui.compositions.RegisterComposition
+import org.akai.pluryrenatlisapp.ui.compositions.WelcomeComposition
 
 @Composable
 fun MainNavHost(
+    preferences: SharedPreferences = LocalContext.current.getSharedPreferences(
+        "authorization",
+        Context.MODE_PRIVATE
+    ),
     startDestination: String = "welcome",
+    modifier: Modifier,
 ) {
     var authorization by remember { mutableStateOf<Authorization?>(null) }
     var currentUser by remember { mutableStateOf<User?>(null) }
@@ -26,41 +36,54 @@ fun MainNavHost(
     val navController = rememberNavController()
     NavHost(
         navController = navController,
-        startDestination = startDestination
+        startDestination = startDestination,
+        modifier = modifier
     ) {
         composable("home") {
-            TODO("Create button actions")
             HomeComposition(
                 user = currentUser!!,
                 rentButtonOnClick = { navController.navigate("rent") }
             )
         }
         composable("login") {
-            val userDetailsDownloadCoroutine = rememberCoroutineScope()
             LoginComposition {
                 authorization = it
-                userDetailsDownloadCoroutine.launch {
-                    TODO("API client call")
-                    navController.navigate("home") {
-                        popUpTo("login") { inclusive = true }
-                    }
+                TODO("API client call")
+                navController.navigate("home") {
+                    popUpTo("login") { inclusive = true }
                 }
             }
         }
         composable("register") {
-            val userDetailsDownloadCoroutine = rememberCoroutineScope()
             RegisterComposition {
                 authorization = it
-                userDetailsDownloadCoroutine.launch {
-                    TODO("API client call")
-                    navController.navigate("home") {
-                        popUpTo("register") { inclusive = true }
-                    }
+                navController.navigate("home") {
+                    popUpTo("register") { inclusive = true }
                 }
             }
         }
         composable("welcome") {
-            TODO("Create welcome screen")
+            if (preferences.contains("email")) {
+                val authorizationVM: AuthorizationViewModel = viewModel()
+                authorizationVM.authorize(
+                    identification = preferences.getString("email", "")!!,
+                    onSuccess = {
+                        val token = authorizationVM.token.value
+                        if (token?.isNotEmpty() == true)
+                            preferences.edit().putString("token", token).apply()
+
+                        authorization = Authorization(token!!)
+                        navController.navigate("home") {
+                            popUpTo("welcome") { inclusive = true }
+                        }
+                    }
+                )
+            }
+
+            WelcomeComposition(
+                loginOnClick = { navController.navigate("login") },
+                registerOnClick = { navController.navigate("register") }
+            )
         }
         composable("rent") {
             TODO("Create rent screen")
